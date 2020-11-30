fc5948677fb9190b3c121214ff1f067be63612dd
Elizabeth Ramos <traderramos3@gmail.com>
2020-11-30 16:48:11 -0500

    class properties

--- src/Components/Coin/coin.jsx
+++ src/Components/Coin/coin.jsx
@@ -8,24 +8,13 @@ import styled from "styled-components";
 `;
 
 export default class Coin extends Component { 
-    constructor(props) {
-        super(props);     
-        this.handleClick = this.handleClick.bind(this);  
-        }
-        
     
-        handleClick(event) {
+        
+        handleClick= (event) => {
         event.preventDefault();
 
         this.props.handleRefresh(this.props.ticker);
-/*     
-        const randomPercentage = 0.995 + Math.random() * 0.01;
-        this.setState(function(oldState) {
-            return {
-                price: oldState.price * randomPercentage
-            };
-        });
- */       
+
     }
     
      render() {
@@ -34,6 +23,7 @@ export default class Coin extends Component {
                 <Td>{this.props.name}</Td>
                 <Td>{this.props.ticker}</Td>
                 <Td>${this.props.price}</Td>
+                {this.props.showBalance ? <Td>{this.props.balance}</Td> : null}
                 <Td>
                     <form action="#" method="POST">
                         <button onClick={this.handleClick}>Refresh</button>
